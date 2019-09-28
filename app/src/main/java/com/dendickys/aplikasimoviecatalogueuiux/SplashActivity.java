6468e6fc8b2713719bdd85645551126f04d051f4
dendickys <febri.unquad2014@gmail.com>
2019-09-29 05:56:04 +0700

    Change icon and application name

--- app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/SplashActivity.java
+++ app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/SplashActivity.java
@@ -2,6 +2,7 @@ package com.dendickys.aplikasimoviecatalogueuiux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,6 +10,19 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2400);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
     }
 }
