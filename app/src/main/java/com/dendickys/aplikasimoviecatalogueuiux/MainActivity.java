a6b92ee1d5fcf016bd620537e9eedc9050ae22b5
dendickys <febri.unquad2014@gmail.com>
2019-09-29 06:31:14 +0700

    Add Bottom Navigation Menu finish

--- app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/MainActivity.java
+++ app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/MainActivity.java
@@ -1,8 +1,15 @@
 package com.dendickys.aplikasimoviecatalogueuiux;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.AppBarConfiguration.Builder;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_movies, R.id.navigation_tv_show).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
