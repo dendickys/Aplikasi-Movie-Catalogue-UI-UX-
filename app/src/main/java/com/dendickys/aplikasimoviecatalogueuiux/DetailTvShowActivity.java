67ff4cf6953914504646b07b89cffd95375434a9
dendickys <febri.unquad2014@gmail.com>
2019-09-29 11:05:59 +0700

    Add xml for detail movies and tv show

---
+++ app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/DetailTvShowActivity.java
@@ -0,0 +1,14 @@
+package com.dendickys.aplikasimoviecatalogueuiux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailTvShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tv_show);
+    }
+}
