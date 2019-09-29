255176d7bdb862134e51f502683ff836c0388fb8
dendickys <febri.unquad2014@gmail.com>
2019-09-29 07:08:18 +0700

    Add Glide dependencies

--- app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/adapter/MoviesAdapter.java
+++ app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/adapter/MoviesAdapter.java
@@ -1,16 +1,35 @@
 package com.dendickys.aplikasimoviecatalogueuiux.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.dendickys.aplikasimoviecatalogueuiux.R;
+import com.dendickys.aplikasimoviecatalogueuiux.model.Movies;
+
+import java.util.ArrayList;
+
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder> {
+
+    private ArrayList<Movies> listMovies;
+    private OnItemClickCallback onItemClickCallback;
+
+    public MoviesAdapter(OnItemClickCallback onItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    public MoviesAdapter(ArrayList<Movies> listMovies) {
+        this.listMovies = listMovies;
+    }
+
     @NonNull
     @Override
     public MoviesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movies, parent, false);
+        return new MoviesViewHolder(view);
     }
 
     @Override
@@ -27,5 +46,9 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesView
         public MoviesViewHolder(@NonNull View itemView) {
             super(itemView);
         }
+    }
+
+    public interface OnItemClickCallback {
+        void onItemClicked(Movies data);
     }
 }
