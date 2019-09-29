3be6cd10cb0ed8c39bd3c57794671c0252b99855
dendickys <febri.unquad2014@gmail.com>
2019-09-29 09:49:40 +0700

    Add list Movies

--- app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/adapter/MoviesAdapter.java
+++ app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/adapter/MoviesAdapter.java
@@ -3,10 +3,14 @@ package com.dendickys.aplikasimoviecatalogueuiux.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.dendickys.aplikasimoviecatalogueuiux.R;
 import com.dendickys.aplikasimoviecatalogueuiux.model.Movies;
 
@@ -34,17 +38,35 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesView
 
     @Override
     public void onBindViewHolder(@NonNull MoviesViewHolder holder, int position) {
+        Movies movies = listMovies.get(position);
 
+        Glide.with(holder.itemView.getContext())
+                .load(movies.getPoster())
+                .apply(new RequestOptions()).override(100, 150)
+                .into(holder.posterMovies);
+        holder.nameMovies.setText(movies.getName());
+        holder.yearMovies.setText(movies.getYear());
+        holder.genreMovies.setText(movies.getGenre());
+        holder.overviewMovies.setText(movies.getDescription());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listMovies.size();
     }
 
-    public class MoviesViewHolder extends RecyclerView.ViewHolder {
-        public MoviesViewHolder(@NonNull View itemView) {
+    class MoviesViewHolder extends RecyclerView.ViewHolder {
+        private ImageView posterMovies;
+        private TextView nameMovies, yearMovies, genreMovies, overviewMovies;
+
+        MoviesViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            posterMovies = itemView.findViewById(R.id.img_poster_movies);
+            nameMovies = itemView.findViewById(R.id.tv_name_movies);
+            yearMovies = itemView.findViewById(R.id.tv_year_movies);
+            genreMovies = itemView.findViewById(R.id.tv_genre_movies);
+            overviewMovies = itemView.findViewById(R.id.tv_overview_movies);
         }
     }
 
--- app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/ui/movies/MoviesFragment.java
+++ app/src/main/java/com/dendickys/aplikasimoviecatalogueuiux/ui/movies/MoviesFragment.java
@@ -1,21 +1,33 @@
 package com.dendickys.aplikasimoviecatalogueuiux.ui.movies;
 
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dendickys.aplikasimoviecatalogueuiux.R;
+import com.dendickys.aplikasimoviecatalogueuiux.adapter.MoviesAdapter;
+import com.dendickys.aplikasimoviecatalogueuiux.model.Movies;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MoviesFragment extends Fragment {
 
+    private String[] dataName, dataYear, dataGenre, dataOverview;
+    private TypedArray dataPoster;
+    private ArrayList<Movies> moviesArrayList;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -29,4 +41,37 @@ public class MoviesFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_movies, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rvMovies = view.findViewById(R.id.rv_movies);
+        rvMovies.setHasFixedSize(true);
+        prepare();
+        addItem();
+        rvMovies.setLayoutManager(new LinearLayoutManager(getContext()));
+        MoviesAdapter moviesAdapter = new MoviesAdapter(moviesArrayList);
+        rvMovies.setAdapter(moviesAdapter);
+    }
+
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name_movies);
+        dataYear = getResources().getStringArray(R.array.data_year_movies);
+        dataGenre = getResources().getStringArray(R.array.data_genre_movies);
+        dataOverview = getResources().getStringArray(R.array.data_overview_movies);
+        dataPoster = getResources().obtainTypedArray(R.array.data_poster_movies);
+    }
+
+    private void addItem() {
+        moviesArrayList = new ArrayList<>();
+        for (int i = 0; i < dataName.length; i++) {
+            Movies movies = new Movies();
+            movies.setName(dataName[i]);
+            movies.setYear(dataYear[i]);
+            movies.setGenre(dataGenre[i]);
+            movies.setDescription(dataOverview[i]);
+            movies.setPoster(dataPoster.getResourceId(i, -1));
+            moviesArrayList.add(movies);
+        }
+    }
 }
